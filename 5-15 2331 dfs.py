40eaf9263b6fa30e92ae8e57f05837efd36b5a0b
hwj20 <912299489@qq.com>
2024-05-15 20:26:18 -0700

    Commit on 05/15/2024

---
+++ 5-15 2331 dfs.py
@@ -0,0 +1,47 @@
+'''
+2331. Evaluate Boolean Binary Tree
+Easy
+
+You are given the root of a full binary tree with the following properties:
+
+Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+The evaluation of a node is as follows:
+
+If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.
+'''
+
+'''
+dfs
+'''
+
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def evaluateTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def dfs(now):
+            if now.left == None:
+                return True if now.val == 1 else False
+            l = dfs(now.left)
+            r = dfs(now.right)
+            if now.val == 2:
+                return l or r
+            else:
+                return l and r
+        return dfs(root)
