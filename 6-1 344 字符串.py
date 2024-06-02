d2b98429bc8650fb218456aac315c7186a67b451
hwj20 <912299489@qq.com>
2024-06-01 21:21:29 -0700

    Commit on 06/01/2024

---
+++ 6-1 344 字符串.py
@@ -0,0 +1,15 @@
+'''
+344
+Easy
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+'''
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        return s.reverse()
