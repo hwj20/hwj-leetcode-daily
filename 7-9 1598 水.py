d882aaddaf4977253b3a27f82262cf4dc235a65b
hwj20 <912299489@qq.com>
2024-07-10 14:47:15 -0700

    Commit on 07/10/2024

---
+++ 7-9 1598 水.py
@@ -0,0 +1,30 @@
+'''
+1598
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+"./" : Remain in the same folder.
+"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+'''
+
+'''
+水一下
+'''
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        ans = 0
+        for l in logs:
+            if l == '../':
+                ans = max(0,ans-1)
+            elif l == './':
+                continue
+            else:
+                ans += 1
+        return ans
