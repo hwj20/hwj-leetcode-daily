e2199861f11246a49b60e737db61d89065f0fb24
hwj20 <912299489@qq.com>
2024-07-15 22:38:28 -0700

    Commit on 07/15/2024

---
+++ 7-15 2096 原始LCA-1.py
@@ -0,0 +1,40 @@
+'''
+2096
+You are given the root of a binary tree with n nodes. Each node is uniquely assigned a value from 1 to n. You are also given an integer startValue representing the value of the start node s, and a different integer destValue representing the value of the destination node t.
+
+Find the shortest path starting from node s and ending at node t. Generate step-by-step directions of such path as a string consisting of only the uppercase letters 'L', 'R', and 'U'. Each letter indicates a specific direction:
+
+'L' means to go from a node to its left child node.
+'R' means to go from a node to its right child node.
+'U' means to go from a node to its parent node.
+Return the step-by-step directions of the shortest path from node s to node t.
+'''
+
+'''
+dfs 直接搞定（x）今天搬家把脑袋都搬没了，抄个答案，这个解好妙
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        def find(n: TreeNode, val: int, path: List[str]) -> bool:
+            if n.val == val:
+                return True
+            if n.left and find(n.left, val, path):
+                path += "L"
+            elif n.right and find(n.right, val, path):
+                path += "R"
+            return path
+
+        s, d = [], []
+        find(root, startValue, s)
+        find(root, destValue, d)
+        while len(s) and len(d) and s[-1] == d[-1]:
+            s.pop()
+            d.pop()
+        return "".join("U" * len(s)) + "".join(reversed(d))
